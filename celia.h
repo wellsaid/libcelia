e2b74a84a3fe85159bd199f0c3dc2c1b495d8cd5
wellsaid <benedetto.girgenti@protonmail.com>
2019-01-17 11:19:12 +0100

    Small fix in header file + removed unneeded ESP32 header from library

--- celia.h
+++ celia.h
@@ -20,6 +20,8 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+#include <pbc.h>
+
 #if defined (__cplusplus)
 extern "C" {
 #endif
--- misc.c
+++ misc.c
@@ -24,7 +24,6 @@
 #include <string.h>
 #include <pbc.h>
 #include <mbedtls/aes.h>
-#include <esp_heap_caps.h>
 
 #include "celia.h"
 
